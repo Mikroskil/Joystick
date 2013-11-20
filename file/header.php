ee341adab692336f011eb560cff128cb4648e7d5
Andreas <andreas_chan1993@yahoo.co.id>
2013-11-21 01:23:49 +0700

    Modifikasi header(2)

--- file/header.php
+++ file/header.php
@@ -1,24 +1,6 @@
 			<link rel="stylesheet" href="../css/templates.css" type="text/css" />
-			<script type="text/javascript">
-				function mAtas()
-				{
-					document.getElementById("headerlogin").className = "headerloginhover";
-				}
-				function mKeluar()
-				{
-					document.getElementById("headerlogin").className = "headerlogin";
-				}
-				function idpassfocus(x)
-				{
-					document.getElementById(x).className = "hltextfocus";
-				}
-				function idpasslostfocus(x)
-				{
-					document.getElementById(x).className = "";
-				}
-			</script>
 			<div class="header" id="header">
-				<div class="headerlogo"><img src=""></img></div>
+				<div class="headerlogo"></div>
 				<div class="headernav">
 					<ul style="margin-left:-40px">
 						<a href="main.html"><li class="headernavmenu">Home</li></a>
@@ -26,22 +8,21 @@
 						<a href="apartment.html"><li class="headernavmenu">Apartement</li></a>
 						<a href="news.html"><li class="headernavmenu">News And Events</li></a>
 						<a href="contact_us.html"><li class="headernavmenu">Contact Us</li></a>
-						<a href="contact_us.html">
-							<li class="headernavmenu" onmouseover="mAtas()">
-								Login
-								<div class="headerlogin" id="headerlogin" onmouseout="mKeluar()">
-									<form>
-										<label>Apartment Number or E-mail</label> <br />
-										<input type="text" id="user_id" onfocus="idpassfocus('user_id')" onblur="idpasslostfocus('user_id')" /><br />
-										<br />
-										<label>Password</label> <br />
-										<input type="password" id="user_pass" onfocus="idpassfocus('user_pass')" onblur="idpasslostfocus('user_pass')"/><br />
-										<br />
-										<input type="button" class="" value="Sign-in" />
-									</form>
-								</div>
-							</li>
-						</a>
+						
+						<li class="headernavmenu">
+							<a href="login.html" style="color:#FFFFFF">Login</a>
+							<div class="headerlogin" id="headerlogin">
+								<form>
+									<label>Apartment Number or E-mail</label> <br />
+									<input type="text" class="hltextfocus"/><br />
+									<br />
+									<label>Password</label> <br />
+									<input type="password" class="hltextfocus"/><br />
+									<br />
+									<input type="button" class="signinbut" value="Sign-in" />
+								</form>
+							</div>
+						</li>
 					</ul>
 				</div>
 			</div>
