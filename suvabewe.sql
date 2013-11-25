d7ea31c9ba62714b5692d3c41c40aedcca4d1aef
Andreas <andreas_chan1993@yahoo.co.id>
2013-11-25 16:57:27 +0700

    Perbaikan SQL

--- suvabewe.sql
+++ suvabewe.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4.1
+-- version 3.5.2.2
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 22, 2013 at 07:57 AM
--- Server version: 5.5.32
--- PHP Version: 5.4.19
+-- Generation Time: Nov 25, 2013 at 10:55 AM
+-- Server version: 5.5.27
+-- PHP Version: 5.4.7
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,27 +17,26 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `test`
+-- Database: `suvabewe`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kamar`
+-- Table structure for table `apartemen`
 --
 
-CREATE TABLE IF NOT EXISTS `kamar` (
-  `no_kamar` int(11) NOT NULL,
-  `password` int(225) NOT NULL,
-  `nama_pemilik` int(50) NOT NULL,
-  `email_address` int(50) NOT NULL,
-  `Available` tinyint(1) NOT NULL,
-  `tagihan_listrik` int(11) NOT NULL,
-  `tagihan_air` int(11) NOT NULL
-PRIMARY KEY (`no_kamar`),
+CREATE TABLE IF NOT EXISTS `apartemen` (
+  `no_kamar` int(5) NOT NULL,
+  `password` text NOT NULL,
+  `nama_pemilik` text NOT NULL,
+  `email` text NOT NULL,
+  `available` tinyint(1) NOT NULL,
+  `tagihan_listrik` int(20) NOT NULL,
+  `tagihan_air` int(20) NOT NULL,
+  PRIMARY KEY (`no_kamar`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
