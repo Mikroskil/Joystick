3b4833dc3b72bb01b1831b5ba9554826fda832b4
lisalie <redixy_lisa@yahoo.co.id>
2013-11-26 09:59:33 +0700

    Penambahan JS untuk slider main.php dan image

--- file/main.php
+++ file/main.php
@@ -2,13 +2,28 @@
 	<head>
 		<title>SUVABEWE | Home</title>
 		<link rel="stylesheet" href="../css/templates.css" type="text/css" />
+		<link type="text/css" rel="stylesheet" href="../css/rhinoslider-1.05.css" />
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+		<script type="text/javascript" src="../js/rhinoslider-1.05.min.js"></script>
+		<script type="text/javascript" src="../js/mousewheel.js"></script>
+		<script type="text/javascript" src="../js/easing.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function() {
+				$('#slider').rhinoslider({
+					effect: 'fade',
+					autoPlay: true,
+				});
+			});
+		</script>
 	</head>
 	<body>
 		<div class="wrapper" id="wrapper">
 			<?php include_once ('header.php');?>
 			<div class="slider">
-					<ul class="slideshow">
-						<li class="slides"><img src="../img/beautiful-sunflower-new_2.jpg" class="slideimg"></li>
+					<ul id="slider" class="slideshow">
+						<li class="slides"><img src="../img/welcome.png" class="slideimg"></li>
+						<li class="slides"><img src="../img/slider/02.jpg" class="slideimg"></li>
+						<li class="slides"><img src="../img/slider/03.jpg" class="slideimg"></li>
 					</ul>
 			</div>
 			<div class="container" style="height:700px">
