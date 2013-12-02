ea300c18869bd95a423aa42c255d047d27987451
laurentstev <laurentstev@yahoo.com>
2013-12-02 11:05:23 +0700

    apartmentdetail

---
+++ file/apartmentdetail.php
@@ -0,0 +1,65 @@
+<html>
+	<head>
+		<title>SUVABEWE | Apartment</title>
+		<link rel="stylesheet" href="../css/templates.css" type="text/css" /></head>
+	<body>
+		<div class="wrapper" id="wrapper">
+			<?php include_once ('header.php');?>
+			<div></div>
+			<div class="container" style="height:700px">
+				<div class="section apartmentdetailsec">
+					<div class="apartmentdetailheader bottomline"><strong>Apartment Detail | Type C</strong></div>
+					<div class="sectioncontent apartmentsecimg">
+						<img src="../img/TypeC.jpg" width="190" height="190" align="middle"/>					</div>
+										<div class="sectioncontent apartmentsecimg">
+						<img align="middle"/>
+					</div>
+					<div class="apartementdetail"><strong> Type C </strong></div>
+				</div>
+				<div class="section apartmentdetailsec">
+									<div class="apartmentdetailheader bottomline"><strong>Specification</strong></div>
+					<div class="specheader">Unit Mix</div>
+					<div class="speccontent">
+						<ul>
+							<li>1 Living Room</li>
+							<li>1 Restroom</li>
+							<li>1 Kitchen</li>
+							<li>1 Bedroom with Single Bed</li>
+						</ul>
+					</div>
+					<div class="specheader">Appliance</div>
+					<div class="speccontent">
+					<ul>
+						<li>Television Set</li>
+						<li>Refrigerator</li>
+						<li>Microwave Oven</li>
+						<li>Air conditioning units with thermo-sensors</li>
+					</ul>
+				</div>
+			</div>
+
+			  <div class="bottomline apartementgambarheader"><strong>Photo Galery </strong></div>
+					<div class="apartementgambar">
+						<table width="940" height="250">
+						<tr align="center">
+						<td width="200" height="180"><img src=""></td>
+						<td width="200"><img src=""></td>
+						<td width="200"><img src=""></td>
+						<td width="200"><img src=""></td>
+						<td width="200"><img src=""></td>
+						</tr>
+						<tr align="center">
+						<td height="30">Living Room </td>
+						<td>Kitchen</td>
+						<td>Dining Room </td>
+						<td>Rest Room </td>
+						<td>Bedroom</td>
+						</tr>
+					  </table>
+					</div>
+
+			</div>
+			<?php include_once ('footer.html');?>
+		</div>
+	</body>
+</html>
