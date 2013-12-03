958bc6f08af837f3b4b18619acd42149ba2d71ba
Sandy <sandy_erey@hotmail.com>
2013-12-03 11:46:32 +0700

    tampilan userinfo tanpa php

---
+++ file/userinfo.php
@@ -0,0 +1,67 @@
+<html>
+	<head>
+		<title>SUVABEWE | User Information</title>
+		<link rel="stylesheet" href="../css/templates.css" type="text/css" />
+	</head>
+	<body>
+		<div class="wrapper" id="wrapper">
+			<?php include_once ('header.php');?>
+			<div class="container confmargin" style="height:900px">
+				<div class="section userinfo">
+					<div class="sectionheader bottomline">
+						<b>User Information</b>
+					</div>
+					<div class="sectioncontent ">
+						<table>
+							<tr>
+								<td>No Kamar</td>
+								<td>:</td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Type Kamar</td>
+								<td>:</td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Spec Kamar</td>
+								<td>:</td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Nama Pemilik</td>
+								<td>:</td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Password</td>
+								<td>:</td>
+								<td></td>
+								<td><input type="button" name="ubah" value="Change"></td>
+							</tr>
+							<tr>
+								<td>Email</td>
+								<td>:</td>
+								<td></td>
+								<td><input type="button" name="ubah" value="Change"></td>
+							</tr>
+							<tr>
+								<td>Tagihan Listrik</td>
+								<td>:</td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Tagihan Air</td>
+								<td>:</td>
+								<td></td>
+							</tr>
+						</table>
+					</div>
+				</div>
+			</div>
+			<?php 
+				include_once ('footer.html'); 
+			?>
+		</div>
+	</body>
+</html>
